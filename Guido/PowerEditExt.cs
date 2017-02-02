7a307a417f760edde1e9d6a99977cafa38837b67
giganoide <giganoide@hotmail.com>
2017-02-02 17:24:13 +0100

    Add project files.

---
+++ Guido/PowerEditExt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Guido
+{
+    public class PowerEditExt
+    {
+        public static IEnumerable<IGrouping<int, KeyValuePair<int, string>>> FindDuplicate(Dictionary<int, string> rows)
+        {
+            var rowsDuplicated = rows.GroupBy(x => x.Value).Where(group => group.Count() > 1).Select(x => x.Key);
+            var result = new Dictionary<int, string>();
+            foreach (var row in rowsDuplicated)
+                foreach (var item in rows.ToList().FindAll(x => x.Value == row))
+                    result.Add(item.Key, item.Value);
+            return result.GroupBy(x => x.Key);
+        }
+    }
+}
---
+++ GuidoTest/PowerEditExtTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guido;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace GuidoTest
+{
+    [TestFixture]
+    public class PowerEditExtTest
+    {
+        static object[] _inputList =
+        {
+            new object[] 
+            {
+                new Dictionary<int, string> {{1, "N100"}, {2, "N200"}, {3, "N300"}},
+                new Dictionary<int, string> ().GroupBy(x => x.Key)
+            },
+            new object[] 
+            {
+                new Dictionary<int, string> {{1, "N100"}, {2, "N100"}, {3, "N300"}},
+                new Dictionary<int, string> {{1, "N100"}, {2, "N100"}}.GroupBy(x => x.Key)
+            },
+            new object[]
+            {
+                new Dictionary<int, string> {{1, "N100"}, {2, "N100"}, {3, "N200"}, {4, "N200"}},
+                new Dictionary<int, string> {{1, "N100"}, {2, "N100"}, {3, "N200"}, {4, "N200"}}.GroupBy(x => x.Key)
+            },
+            new object[]
+            {
+                new Dictionary<int, string> {{1, "N100"}, {2, "N200"}, {3, ""}, {4, "N100" }},
+                new Dictionary<int, string> {{1, "N100"}, {4, "N100"}}.GroupBy(x => x.Key)
+            }
+        };
+        
+        [Test, TestCaseSource(nameof(_inputList))]
+        public void FindDuplicateTest(Dictionary<int, string> input, IEnumerable<IGrouping<int, KeyValuePair<int, string>>> expected)
+        {
+            Assert.AreEqual(expected, PowerEditExt.FindDuplicate(input));
+        }
+    }
+}
